c4e9a6c72414dde6fd032249072685bac24c8dbd
Julien Bravo <bravojulien4@gmail.com>
2018-11-23 17:16:58 +0100

    modification on target

--- sls-villains/getTarget.js
+++ sls-villains/getTarget.js
@@ -2,8 +2,21 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 const dbVillains = require('./db-villains').dbVillains;
 
+
+const getTargets = () => dbVillains.find({}).exec();
+
+
 const getTargetHandler = async msg => {
     const body = JSON.parse(msg.body);
+
+    const mapTargets = getTargets.map( villain => {
+        // delete the details if villains coordinates are equal to heroes coordinates
+        if ( villain.x === body['x'])
+            if ( villain.y === body['y'])
+                const details = { "name": villain.name }
+
+
+    });
 
     return ({
         status: 200,
